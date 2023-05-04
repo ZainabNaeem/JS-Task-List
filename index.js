384e5746bab0ef542eaaa4943ff19b6cfa29d135
ZainabNaeem <130337110+ZainabNaeem@users.noreply.github.com>
2023-05-04 08:30:09 +0000

    refctoring JS code

--- index.js
+++ index.js
@@ -1,44 +1,70 @@
-import tasks from "./tasks.json";
-console.log(tasks);
+import tasks from "./tasks.json" assert { type: "json" };
 
-for (var i = 0; i < tasklist.length; i++) {
-  var li = document.createElement("li");
-  var link = document.createElement("a");
-  var textnode = document.createTextNode(tasklist[i].name);
-  link.setAttribute("href", tasklist[i].href);
-  link.setAttribute("target", "iframes-list");
-  link.appendChild(textnode);
-  li.appendChild(link);
-
-  document.getElementById("submenu").appendChild(li);
+function displayList() {
+  for (let i = 0; i < tasks.length; i++) {
+    let obj = tasks[i];
+    console.log(obj.name);
+    const list = document.createElement("li");
+    const link = document.createElement("a");
+    link.setAttribute("href", obj.href);
+    link.setAttribute("target", "task-iframe");
+    link.innerHTML = obj.name;
+    list.appendChild(link);
+    document.getElementById("submenu").appendChild(list);
+  }
+  // document.getElementById("menu").innerHTML = list;
+  // console.log(list);
 }
-const ul = document.getElementById("submenu");
-const list = ul.querySelectorAll("li");
+displayList();
+// function displayList() {
+//   const item = document.createElement("li");
+//   const link = document.createElement("a");
+//   link.setAttribute("href", tasks.href);
+//   link.setAttribute("target", "task-iframe");
+//   link.innerHTML = tasks.name;
+//   item.appendChild(link);
+//   document.getElementById("menu").appendChild(item);
+// }
+// ascend_btn.addEventListener("click", displayList);
 
-const Ascend = (list) =>
-  [...list].sort((a, b) => {
-    const A = a.textContent;
-    const B = b.textContent;
-    if (A < B) {
-      return -1;
-    }
-  });
+// for (var i = 0; i < tasks.length; i++) {
+//   var li = document.createElement("li");
+//   var link = document.createElement("a");
+//   var textnode = document.createTextNode(tasks[i].name);
+//   link.setAttribute("href", tasks[i].href);
+//   link.setAttribute("target", "iframes-list");
+//   link.appendChild(textnode);
+//   li.appendChild(link);
 
-const Decend = (list) =>
-  [...list].sort((a, b) => {
-    const A = a.textContent;
-    const B = b.textContent;
-    if (A > B) {
-      return -1;
-    }
-  });
-var asc = document.getElementById("ascending");
-asc.addEventListener("click", function () {
-  console.log(">>> list:", list);
-  console.log(">>> asc:", ...Ascend(list));
-  ul.append(...Ascend(list));
-});
-var dec = document.getElementById("descending");
-dec.addEventListener("click", function () {
-  ul.append(...Decend(list));
-});
+//   document.getElementById("submenu").appendChild(li);
+// }
+// const ul = document.getElementById("submenu");
+// const list = ul.querySelectorAll("li");
+
+// const Ascend = (list) =>
+//   [...list].sort((a, b) => {
+//     const A = a.textContent;
+//     const B = b.textContent;
+//     if (A < B) {
+//       return -1;
+//     }
+//   });
+
+// const Decend = (list) =>
+//   [...list].sort((a, b) => {
+//     const A = a.textContent;
+//     const B = b.textContent;
+//     if (A > B) {
+//       return -1;
+//     }
+//   });
+// var asc = document.getElementById("ascending");
+// asc.addEventListener("click", function () {
+//   console.log(">>> list:", list);
+//   console.log(">>> asc:", ...Ascend(list));
+//   ul.append(...Ascend(list));
+// });
+// var dec = document.getElementById("descending");
+// dec.addEventListener("click", function () {
+//   ul.append(...Decend(list));
+// });
