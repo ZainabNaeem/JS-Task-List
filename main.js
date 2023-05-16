29370f995d6aca1a8f5902415cca89606028232e
ZainabNaeem <130337110+ZainabNaeem@users.noreply.github.com>
2023-05-16 18:49:01 +0000

    refactored  JS

--- main.js
+++ main.js
@@ -2,28 +2,10 @@ import tasks from "./tasks.json" assert { type: "json" };
 
 const list = document.getElementById("menu");
 const searchBar = document.getElementById("searchBar");
-const compareFn = (a, b) => {
-  if (a.id < b.id) {
-    return -1;
-  } else if (b.id < a.id) {
-    return 1;
-  } else {
-    return 0;
-  }
-};
 
-let order = 1;
-
-document.querySelector("#descending").addEventListener("click", () => {
-  order = 1;
-});
-
-document.querySelector("#ascending").addEventListener("click", () => {
-  order = -1;
-});
-
+//list display section
 var render_lists = function (lists) {
-  const ItemList = tasks
+  const ItemList = lists
     .map(
       (tasks) =>
         `<li><a href="${tasks.href}" target="iframes-list"> <h4>${tasks.name}</h4></a></li>`
@@ -32,16 +14,31 @@ var render_lists = function (lists) {
   list.innerHTML = ItemList;
 };
 render_lists(tasks);
+//task filter section
 const render = () => {
   searchBar.addEventListener("keyup", (task) => {
     const searchString = task.target.value.toLowerCase();
     const filteredTasks = tasks.filter((task) =>
       task.name.toLowerCase().includes(searchString)
     );
-
-    filteredTasks.sort((a, b) => compareFn(a, b) * order);
-    console.log(filteredTasks);
     render_lists(filteredTasks);
   });
 };
 render();
+//task sorting section
+document.querySelector("#ascending").addEventListener("click", () =>
+  tasks.sort(function (a, b) {
+    if (a.name < b.name) {
+      return -1;
+    }
+    render_lists(tasks);
+  })
+);
+document.querySelector("#descending").addEventListener("click", () =>
+  tasks.sort(function (a, b) {
+    if (a.name > b.name) {
+      return -1;
+    }
+    render_lists(tasks);
+  })
+);
